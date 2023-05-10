ec6507081de7360d4ffdc22152c819166568c339
SameerShetty <kalyanshettisameer@gmail.com>
2023-05-10 15:28:55 +0530

    cloned my own repo

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Ticket() {
-  return (
-    <div
-      style={{ width: "350px", height: "450px", backgroundColor: "#eee" }}
-    ></div>
-  );
+  return <div>Ticket</div>;
 }
 
 export default Ticket;
