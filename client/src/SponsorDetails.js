5f0551467acc51a49c63d04b9bb9c973d7b34a66
SameerShetty <kalyanshettisameer@gmail.com>
2023-06-12 17:34:48 +0530

    done with sponsors logo

--- client/src/SponsorDetails.js
+++ client/src/SponsorDetails.js
@@ -7,5 +7,13 @@ const SponsorDetails = [
     id: 2,
     img: "../imgs/VT.jpg",
   },
+  {
+    id: 3,
+    img: "../imgs/newron.jpg",
+  },
+  {
+    id: 4,
+    img: "../imgs/canara.png",
+  },
 ];
 export default SponsorDetails;
